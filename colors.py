0783a2d0c3c4d63b90c2b3869da3fe8a67b28368
Qu1nel <quinelleave@gmail.com>
2021-08-13 13:24:01 +0300

    files with all vars and colors of the game

---
+++ colors.py
@@ -0,0 +1,13 @@
+""" Just RGB colors in tuples :3 """
+
+BG_COLOR = (155, 89, 182)
+BG_LINE_COLOR = (142, 68, 173)
+CIRCLE_COLOR = (149, 165, 166)
+CROSS_COLOR = (66, 66, 66)
+
+RED = (255, 0, 0)
+GREEN = (0, 255, 00)
+BLUE = (0, 0, 255)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GRAY = (128, 128, 128)
---
+++ config.py
@@ -0,0 +1,22 @@
+# --------------
+# All graphic components of the game depending on the screen size (Width and Height)
+# P.S.(preferably the same value is a multiple of 3)
+# --------------
+SC_WIDTH = 600
+SC_HEIGHT = 600
+
+CAPTION = 'TicTacToe'
+
+FRAME_RATE = 120
+
+BOARD_COLS = 3
+BOARD_ROWS = 3
+
+CIRCLE_RADIUS = int(SC_WIDTH * 0.1)  # 600px -- 10% = 60px
+CROSS_WIDTH = int(SC_WIDTH * 0.0417)  # 600px -- 4.17% = 25px default
+CIRCLE_WIDTH = int(SC_WIDTH * 0.025)  # 600px -- 2.5% = 15px default
+LINE_WIDTH = CROSS_WIDTH
+WIN_LINE_WIDTH = CIRCLE_WIDTH
+
+SPACE = int(SC_WIDTH * 0.0917)  # 600px -- 9.17% = 55px default
+SQUARE_SIZE = SC_WIDTH // 3  # only 3 cells on the field lol
