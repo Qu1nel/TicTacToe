8089d5e93957f7b302b686656c967741f02bace6
Qu1nel <quinelleave@gmail.com>
2021-08-14 10:58:29 +0300

    method handle_events

--- TicTacToe.py
+++ TicTacToe.py
@@ -15,6 +15,20 @@ class TicTacToe(Interface):
         super().__init__()
         self.board = GameBoard()
 
+    def handle_events(self) -> None:
+        """Handles actions entered by the player"""
+        for event in pygame.event.get():
+
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pass
+
+            if event.type == pygame.KEYDOWN:
+                pass
+
     def run(self) -> None:
         """Launches the game"""
         self.draw_BG()
@@ -22,7 +36,7 @@ class TicTacToe(Interface):
             pygame.display.update()
 
             # self.computer()
-            # self.handle_events()
+            self.handle_events()
 
             self.clock.tick(self.framerate)
 
