a5878a1f00c62dc8fd7a0f443b10188cb4c2b466
Qu1nel <quinelleave@gmail.com>
2021-08-14 10:06:30 +0300

    Init the main game class tictactoe

---
+++ TicTacToe.py
@@ -0,0 +1,21 @@
+import sys
+import math
+import time
+import random
+import pygame
+import numpy as np
+import config as c
+from interface import Interface
+from board import GameBoard
+
+
+class TicTacToe(Interface):
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
