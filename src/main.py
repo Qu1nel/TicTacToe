4712a24afaf64db78a5954e34d643ef64db344c7
Ivan Kovach <eeiprex@gmail.com>
2023-07-09 03:38:07 +0300

    [refact] Total refactoring `main.py`
    Refactoring as recommended by ruff

--- src/main.py
+++ src/main.py
@@ -1,9 +1,6 @@
-import math
-import random
 import sys
-import time
+from typing import Final
 
-import numpy as np
 import pygame
 
 from src.board import GameBoard
@@ -11,10 +8,15 @@ from src.config import GO_FIRST
 from src.interface import Interface
 from src.player import ComputerPlayer, HumanPlayer
 
+CROSS: Final[int] = 2
+CIRCLE: Final[int] = 1
+
 
 class TicTacToe(Interface):
+    """The main class game Tic Tac Toe."""
 
-    def __init__(self):
+    def __init__(self) -> None:
+        """Init TicTacToe instance."""
         super().__init__()
         self.go_first = GO_FIRST
         self.human_player = HumanPlayer()
@@ -22,50 +24,52 @@ class TicTacToe(Interface):
         self.board = GameBoard()
 
     def draw_figures(self) -> None:
-        """Draws shapes (cross, circle) relative to the state of the board"""
+        """Draws shapes (cross, circle) relative to the state of the board."""
         for row in range(self.board.rows):
             for col in range(self.board.cols):
-                if self.board[row][col] == 1:
+                if self.board[row][col] == CIRCLE:
                     self._draw_circle(row, col)
-                elif self.board[row][col] == 2:
+                elif self.board[row][col] == CROSS:
                     self._draw_cross(row, col)
 
-    def check_win(self, player: int, draw=True) -> bool:
-        """The function determines whether there has been a victory
-        and draws a victory line for the winning piece using the flag.
+    def check_win(self, player: int, draw: bool = True) -> bool:
+        """Draws a line in case of victory.
 
-        :param player: The player who made the move (int)
-        :param draw: A flag that determines whether to draw when a victory is detected (bool)
-        :return: bool
+        Args:
+        ----
+        player (int): The player who made the move
+        draw (bool): A flag that determines whether to draw when a
+            victory is detected (bool)
+
+        Returns:
+        -------
+            The True if else False
+
         """
         for col in range(self.board.cols):  # vertical win check
-            if (self.board[0][col], self.board[1][col], self.board[2][col]) == (player, player, player):
-                if draw:
-                    self._draw_vertical_winning_line(col, player)
+            if (self.board[0][col], self.board[1][col], self.board[2][col]) == (player, player, player) and draw:
+                self._draw_vertical_winning_line(col, player)
                 return True
 
         for row in range(self.board.rows):  # horizontal win check
-            if (self.board[row][0], self.board[row][1], self.board[row][2]) == (player, player, player):
-                if draw:
-                    self._draw_horizontal_winning_line(row, player)
+            if (self.board[row][0], self.board[row][1], self.board[row][2]) == (player, player, player) and draw:
+                self._draw_horizontal_winning_line(row, player)
                 return True
 
         # asc diagonal win check
-        if (self.board[2][0], self.board[1][1], self.board[0][2]) == (player, player, player):
-            if draw:
-                self._draw_left_diagonal_line(player)
+        if (self.board[2][0], self.board[1][1], self.board[0][2]) == (player, player, player) and draw:
+            self._draw_left_diagonal_line(player)
             return True
 
         # desc diagonal win chek
-        if (self.board[0][0], self.board[1][1], self.board[2][2]) == (player, player, player):
-            if draw:
-                self._draw_right_diagonal_line(player)
+        if (self.board[0][0], self.board[1][1], self.board[2][2]) == (player, player, player) and draw:
+            self._draw_right_diagonal_line(player)
             return True
 
         return False
 
     def restart(self) -> None:
-        """Launches a new game"""
+        """Launch a new game."""
         self.draw_BG()
         self.board = GameBoard()
         self.game_over = False
@@ -73,7 +77,7 @@ class TicTacToe(Interface):
         self.go_first = GO_FIRST
 
     def update(self, row: int, col: int, number_player: int) -> None:
-        """Updates the table, renders it and checks if there is a victory"""
+        """Update the table, renders it and checks if there is a victory."""
         if self.board.available_square(row, col):
             self.board.mark_square(row, col, number_player)
             if self.check_win(number_player, draw=True):
@@ -82,7 +86,7 @@ class TicTacToe(Interface):
             self.draw_figures()
 
     def handle_events(self) -> None:
-        """Handles actions entered by the player"""
+        """Handle actions entered by the player."""
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -94,18 +98,18 @@ class TicTacToe(Interface):
                 if self.board.available_square(clock_row, clock_col):
                     self.human_player.make_move(self, clock_row, clock_col)
 
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_r:
-                    self.restart()
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_r:
+                self.restart()
 
     def make_moves(self) -> None:
-        if self.go_first == 'COMPUTER' and not self.game_over:
+        """Chooses who makes the move."""
+        if self.go_first == "COMPUTER" and not self.game_over:
             self.computer_player.make_move(self)
-        elif self.go_first == 'PLAYER':
+        elif self.go_first == "PLAYER":
             self.handle_events()
 
     def run(self) -> None:
-        """Launches the game"""
+        """Launch the game."""
         self.draw_BG()
         while True:
             self.make_moves()
@@ -114,6 +118,6 @@ class TicTacToe(Interface):
             self.clock.tick(self.framerate)
 
 
-def main():
+def main() -> None:
+    """Enter point in game."""
     TicTacToe().run()
-
