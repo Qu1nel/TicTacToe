212172c07597a59b46d291505e1b545f4d9fc7a7
Ivan Kovach <eeiprex@gmail.com>
2023-08-15 16:27:46 +0300

    [feat#__init__.py] Add application icon and title
    A feature has also been added to fix the paths to requested
    resources to further compile the application into an exe without
    problems.

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from src.main import main
+from src import main
 
 if __name__ == "__main__":
     main()
--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,34 @@
 """Package providing all the necessary packages for a game of tic-tac-toe."""
+import sys
+from pathlib import Path
+
+import pygame as pg
+
+from src.main import TicTacToe
+
+
+def resource_path(relative_path: Path) -> Path:
+    """Function for working paths inside an exe for python."""
+    base_path = Path(getattr(sys, "_MEIPASS", ".")).absolute()
+    return base_path.joinpath(relative_path)
+
+
+pg.init()
+
+icon_name = "icon.png"
+icon_folder_name = "icons"
+icon_path = Path(icon_folder_name) / Path(icon_name)
+
+try:
+    icon = pg.image.load(resource_path(icon_path))
+    pg.display.set_icon(icon)
+except FileNotFoundError:
+    pass
+
+pg.display.set_caption("TicTacToe")
+
+
+def main() -> None:
+    """Entry point to the game."""
+    game = TicTacToe()
+    game.run()
